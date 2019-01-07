3769c93df8c9ad56c65119d8e6dcb78ed7b4e15b
Cooper Chen <cooper.chen@titansoft.com.sg>
2019-01-08 00:48:28 +0700

    AverageString

---
+++ StringAverage/Kata001.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAverage
+{
+    public class Kata001
+    {
+        public static string AverageString(string str)
+        {
+            // Code away
+            string[] arrNumber = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            string[] arrValue = str.Split(' ');
+            int inputCount = arrValue.Length;
+            int sumOfInput = 0;
+            int avgOfInput = 0;
+            bool outOfRange = true;
+
+            if (inputCount == 0)
+            {
+                return "n/a";
+            }
+
+            foreach (string X in arrValue)
+            {
+                outOfRange = true;
+                for (int i = 0; i < arrNumber.Length; i++)
+                {
+                    if (X == arrNumber[i])
+                    {
+                        sumOfInput += i;
+                        outOfRange = false;
+                    }
+                }
+                if (outOfRange == true)
+                {
+                    return "n/a";
+                }
+            }
+
+            if (sumOfInput == 0)
+            {
+                return arrNumber[0];
+            }
+            else
+            {
+                avgOfInput = sumOfInput / inputCount;
+                return arrNumber[avgOfInput];
+            }
+        }
+    }
+}
---
+++ StringAverageTests/Kata001Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringAverage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAverage.Tests
+{
+    [TestClass()]
+    public class Kata001Tests
+    {
+        [TestMethod()]
+        public void AverageStringTest()
+        {
+            Assert.AreEqual("four", Kata001.AverageString("zero nine five two"));
+            Assert.AreEqual("three", Kata001.AverageString("four six two three"));
+            Assert.AreEqual("three", Kata001.AverageString("one two three four five"));
+            Assert.AreEqual("four", Kata001.AverageString("five four"));
+            Assert.AreEqual("zero", Kata001.AverageString("zero zero zero zero zero"));
+            Assert.AreEqual("two", Kata001.AverageString("one one eight one"));
+            Assert.AreEqual("n/a", Kata001.AverageString(""));
+        }
+    }
+}
